e84a20be3bc3f6b60a0748d4662b2679cb8bb8b5
kohantikanath <kohan.nath@gmail.com>
2024-09-26 15:49:03 +0530

    Added functionality to Bishop

--- public/javascript/chess/pieces/bishop.js
+++ public/javascript/chess/pieces/bishop.js
@@ -1,26 +1,81 @@
-var Bishop = function(config){
-    this.type = 'bishop';
-    this.constructor(config);
+var Bishop = function (config) {
+  this.pieceType = "bishop";
+  this.initialize(config);
 };
 
+Bishop.prototype = Object.create(Piece.prototype);
+Bishop.prototype.constructor = Bishop;
 
+Bishop.prototype.move = function (destination) {
+  if (!this.isMoveValid(destination)) {
+    console.log("Invalid move: This move is not allowed for a bishop.");
 
-Bishop.prototype = new Piece({});
-Bishop.prototype.moveTo = function(targetPosition) {
-    const currentCol = this.position[0].charCodeAt(0);
-    const currentRow = parseInt(this.position[1]);
-    const targetCol = targetPosition.col.charCodeAt(0);
-    const targetRow = parseInt(targetPosition.row);
+    return;
+  }
+  console.log("Executing move for the bishop.");
+  var newPosition = destination.col + destination.row;
+  this.position = newPosition;
+  this.display();
+  console.log("Move completed.");
+};
 
-    const colDiff = Math.abs(currentCol - targetCol);
-    const rowDiff = Math.abs(currentRow - targetRow);
+Bishop.prototype.isMoveValid = function (destination) {
+  var currentRow = parseInt(this.position[1], 10);
+  var destinationRow = parseInt(destination.row, 10);
+  var currentCol = this.position[0].toUpperCase();
+  var destinationCol = destination.col.toUpperCase();
 
-    if (colDiff === rowDiff && colDiff > 0) {
-        if (window.chessBoard.isPathClear(this.position, targetPosition)) {
-            this.position = targetPosition.col + targetPosition.row;
-            this.render();
-            return true;
-        }
-    }
+  console.log("Current Row: ", currentRow + " " + typeof currentRow);
+  console.log(
+    "Destination Row: ",
+    destinationRow + " " + typeof destinationRow
+  );
+  console.log("Current Col: ", currentCol + " " + typeof currentCol);
+  console.log(
+    "Destination Col: ",
+    destinationCol + " " + typeof destinationCol
+  );
+
+  // Check diagonal move
+  if (
+    Math.abs(currentRow - destinationRow) !==
+    Math.abs(currentCol.charCodeAt(0) - destinationCol.charCodeAt(0))
+  ) {
+    console.log("Invalid move: Bishop can only move diagonally.");
     return false;
+  }
+
+  var rowDirection = destinationRow > currentRow ? 1 : -1;
+  var colDirection =
+    destinationCol.charCodeAt(0) > currentCol.charCodeAt(0) ? 1 : -1;
+
+  var nextRow = currentRow + rowDirection;
+  var nextCol = currentCol.charCodeAt(0) + colDirection;
+
+  // Check if path is clear
+  while (
+    nextRow !== destinationRow &&
+    nextCol !== destinationCol.charCodeAt(0)
+  ) {
+    var intermediateSpot = {
+      row: nextRow.toString(),
+      col: String.fromCharCode(nextCol),
+    };
+
+    if (this.Board.getPieceAt(intermediateSpot)) {
+      console.log("Invalid move: Another piece blocks the path.");
+      return false;
+    }
+
+    nextRow += rowDirection;
+    nextCol += colDirection;
+  }
+
+  return true;
 };
+
+function limitExecution(fn, limit) {
+  return function () {
+    return fn(limit);
+  };
+}
